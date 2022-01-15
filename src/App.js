6458239d91f32859d68f5bb05f0cfc05b0e85114
Mayowa Ige <damayor16@gmail.com>
2022-01-15 15:30:37 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from "./components/Header/Header";
+import Header from "./components/Footer/Footer";
 
 function App() {
-  return <div className="App"></div>
+  return <div className="App">
+    <hHeader />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>Homepage</div>} />
+      <Route path="/about" element={<div>About</div>} />
+      <Route path="/product" element={<div>Product</div>} />
+      <Route path="/product/:productId" element={<div>Product Details</div>} />      
+    </Routes>
+    </BrowserRouter>
+    <Footer />
+  </div>;
 }
 
 export default App;
