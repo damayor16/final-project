d19699a78bceaab1e26414ec87c304824ae11665
emmanuel <emmanawoyele@gmail.com>
2022-01-17 21:05:29 +0000

    caruosel created without a animation

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,7 +1,18 @@
 import "./Homepage.css"
 function Homepage () {
-    return <h1 class="display-1">Welcome To My Recipes</h1>   
-    
+    return<section>
+<div className="slider-section">
+    <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimagesvc.meredithcorp.io%2Fv3%2Fmm%2Fimage%3Furl%3Dhttps%253A%252F%252Fstatic.onecms.io%252Fwp-content%252Fuploads%252Fsites%252F9%252F2021%252F07%252F13%252FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg&q=85"/>
+    <div className="slider-section-text">
+        <h1>Homemade delicious burger</h1>
+<p>Meat base recipe</p>
+ <div className='slider-section-button'>
+     <a href="\"> see more</a>
+ </div>
+
+    </div>
+</div>
+    </section>
 }
 
 export default Homepage
