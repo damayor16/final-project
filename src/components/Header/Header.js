e44ab17073795a0d5db0911759b45461ab4f4e3e
Mayowa Ige <damayor16@gmail.com>
2022-01-18 15:14:53 +0200

    Slide

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,88 +2,43 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {  
- /* return (    
-    <nav className="navbar navbar-expand-lg navbar-light bg-light navbar_color">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-        <img src="./logo.png" alt="" width="60" height="48"></img>
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/" className="nav-link active" aria-current="page">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-link active" aria-current="page">
-                About
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Recipe
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <Link className="dropdown-item" to="/meat">
-                    Meat
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/vegan">
-                    Vegan
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/desert">
-                    Desert
-                  </Link>
-                </li>                
-                <li>
-                  <Link className="dropdown-item" to="/pastry">
-                    Pastry
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/search">Search</Link>
-            </li>
-          </ul>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
+  return (
+   
+    <nav className=" navbar_container">
+      <div className="navbar_container-logo">
+        <a href="/">MayoRecipes</a>
       </div>
-    </nav>   
-  );*/
+       <div className="navbar_container-menu">
+         <ul>
+         <li>
+           <a href="/" >Home</a>
+         </li>
+      
+         <li>
+         <Link to="/about" className="nav-link active" aria-current="page">
+                About 
+              </Link>
+         </li>
+
+         <li>
+         <Link to="/recipe" className="nav-link active" aria-current="page">
+               Recipes
+              </Link>
+         </li>
+
+         <li>
+         <Link to="/contact" className="nav-link active" aria-current="page">
+                Contact us
+              </Link>
+         </li>
+         </ul>
+       </div>
+       <div className=" navabar_container-search">
+       <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search for new recipe" title="Type in a category"/>
+       </div>
+         </nav>
+ 
+  );
 }
 
 export default Header;
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,7 +1,54 @@
 import "./Homepage.css"
-function Homepage () {
-    return <h1 class="display-1">Welcome To My Recipes</h1>   
-    
+function Homepage() {
+    return <section>
+        <div className="slider-section">
+            <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimagesvc.meredithcorp.io%2Fv3%2Fmm%2Fimage%3Furl%3Dhttps%253A%252F%252Fstatic.onecms.io%252Fwp-content%252Fuploads%252Fsites%252F9%252F2021%252F07%252F13%252FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg&q=85" />
+            <div className="slider-section-text">
+                <h1>Homemade delicious burger</h1>
+                <p>Meat base recipe</p>
+                <div className='slider-section-button'>
+                    <a href="\"> see more</a>
+                </div>
+
+            </div>
+        </div>
+
+        <div className="slider-section">
+            <img src="https://images.unsplash.com/photo-1623428187969-5da2dcea5ebf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" />
+            <div className="slider-section-text">
+                <h1>Homemade Delicious Salad</h1>
+                <p>Vegan base recipe</p>
+                <div className='slider-section-button'>
+                    <a href="\"> see more</a>
+                </div>
+
+            </div>
+        </div>
+
+        <div className="slider-section">
+            <img src="https://images.unsplash.com/photo-1533134242443-d4fd215305ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+            <div className="slider-section-text">
+                <h1>Homemade Delicious Cheesecake</h1>
+                <p>Sweet base recipe</p>
+                <div className='slider-section-button'>
+                    <a href="\"> see more</a>
+                </div>
+
+            </div>
+        </div>
+
+        <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a className="next" onclick="plusSlides(1)">&#10095;</a>
+        <br></br>
+
+        <div style="text-align:center">
+            <span className="dot" onclick="currentSlide(1)"></span>
+            <span className="dot" onclick="currentSlide(2)"></span>
+            <span className="dot" onclick="currentSlide(3)"></span>
+        </div>
+
+
+    </section>
 }
 
 export default Homepage
